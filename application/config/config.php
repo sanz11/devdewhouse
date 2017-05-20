c79448d98745daac3f8a17e25fffd0e7b15dca30
Marlith <yhamilethmaite97@gmail.com>
2017-05-20 17:30:28 -0500

    todo config

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'https://devdew.com/proyects/devdewhome/';
-$config['base_url'] = 'http://localhost/tesischicas/';
+//$config['base_url'] = 'http://localhost/tesischicas/';
+//$config['base_url'] = 'http://localhost/devdewhouse/';
 /*
 |--------------------------------------------------------------------------
 | Index File
